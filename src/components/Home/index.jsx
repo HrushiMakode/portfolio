34a4d286a72162b49814fbcde7c2fe648e12ac84
Hrushikesh Makode <makodehrushikesh@gmail.com>
2021-07-16 14:33:34 +0530

    Home Component

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header, Footer, Body } from "../index";
+import "./home.css";
+
+const Home = () => {
+	return (
+		<div className="home">
+			<Header />
+			<Body />
+			<Footer />
+		</div>
+	);
+};
+
+export default Home;
