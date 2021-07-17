aa3c6dfb15a206a1da652cb5727e13d5096fea3f
Hrushikesh Makode <makodehrushikesh@gmail.com>
2021-07-17 23:18:46 +0530

    contact component is added

--- src/components/Body/Contact/index.jsx
+++ src/components/Body/Contact/index.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./contact.css";
+import Separator from "../../common/Separator";
+import SocialContact from "../../common/SocialContact";
 
 const Contact = () => {
-	return <div>I am Contact</div>;
+	return (
+		<div className="contact">
+			<Separator />
+			<label className="section-title">Contact</label>
+			<div className="contact-container">
+				<div className="contact-left">
+					<p>Want to get in touch ? Contact me on any of the platform</p>
+					<SocialContact />
+				</div>
+				<div className="download">
+					<a
+						href={require("../../../Assets/resume.pdf").default}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<i class="fi-rr-cloud-download download-icon"></i> Download Resume
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
