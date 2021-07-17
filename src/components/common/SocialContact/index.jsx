e84848413d8af64f22dc3f0cb6b52b51875205d6
Hrushikesh Makode <makodehrushikesh@gmail.com>
2021-07-17 14:14:15 +0530

    social contact is added

--- src/components/common/SocialContact/index.jsx
+++ src/components/common/SocialContact/index.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./social-contact.css";
+import { SocialData } from "../../../Assets/data/social";
 
 const SocialContact = () => {
-	return <div>I am Social Contacts</div>;
+	const data = SocialData;
+	return (
+		<div className="social-contact">
+			{data.map((item) => {
+				return (
+					<a href={item.link} target="_blank" rel="noopener noreferrer">
+						<div className="social-icon-div">
+							<img
+								src={item.icon}
+								className="social-icon"
+								alt={item.platform}
+							/>
+						</div>
+					</a>
+				);
+			})}
+		</div>
+	);
 };
 
 export default SocialContact;
