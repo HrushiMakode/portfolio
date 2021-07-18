a8e4e4f81cf8f856f2654b97b6f7ccf129808cee
Hrushikesh Makode <makodehrushikesh@gmail.com>
2021-07-18 23:46:59 +0530

    projects added

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,20 +1,32 @@
 export const ProjectsData = [
 	{
 		id: 1,
-		title: "Weather App",
+		title: "Image Captionner for Visually Impaired",
 		about:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce efficitur cursus efficitur. Integer non lorem ac magna vestibulum molestie. Morbi eget nibh molestie quam eleifend interdum",
-		tags: ["ReactJS", "PWA", "API"],
-		demo: "https://blissful-kilby-93a8cf.netlify.app/",
-		github: "https://github.com/HrushiMakode/PWA-React-App",
-		image: "https://i.ibb.co/hW1T0qb/Screenshot-9.png",
+			"Developed a React Native App to find an audio caption for an image clicked by a visually impaired person, perform the image captioning using pre-trained CNN and LSTM on the VizWiz dataset",
+		tags: ["ReactNative", "Python", "CNN", "LSTM"],
+		demo: "https://expo.io/@hrushimakode/expo-camera-app",
+		github: "https://github.com/HrushiMakode/expo-camera-app",
+		image:
+			"https://blog.crowdbotics.com/content/images/2020/03/React-Native-Featured-Image-1.png",
 	},
 	{
 		id: 2,
+		title: "University Result Analysis Software",
+		about:
+			"Developed a Software for college showing data visualization,trend analysis and generate detailed report for university exam results of students.",
+		tags: ["Java", "JSP", "MYSQL", "Bootstrap", "CharJS"],
+		demo: "",
+		github: "https://github.com/hrushiMakode/RAS",
+		image:
+			"https://jasmingimenez.files.wordpress.com/2015/03/jsp-myql.jpg?w=940",
+	},
+	{
+		id: 3,
 		title: "Weather App",
 		about:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce efficitur cursus efficitur. Integer non lorem ac magna vestibulum molestie. Morbi eget nibh molestie quam eleifend interdum",
-		tags: ["ReactJS", "AWS", "NodeJS", "React Native"],
+			"Installable React Progressive Web Apps (PWA) which uses open weather API for displaying weather of search region",
+		tags: ["ReactJS", "PWA", "API"],
 		demo: "https://blissful-kilby-93a8cf.netlify.app/",
 		github: "https://github.com/HrushiMakode/PWA-React-App",
 		image: "https://i.ibb.co/hW1T0qb/Screenshot-9.png",
