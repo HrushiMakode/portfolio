ef1a07fd4ba763f61695d3101f88bcb7e34d03ea
Hrushikesh Makode <makodehrushikesh@gmail.com>
2021-10-11 20:39:15 +0530

    Projects data updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -26,9 +26,30 @@ export const ProjectsData = [
 		title: "Weather App",
 		about:
 			"Installable React Progressive Web Apps (PWA) which uses open weather API for displaying weather of search region",
-		tags: ["ReactJS", "PWA", "API"],
-		demo: "https://blissful-kilby-93a8cf.netlify.app/",
+		tags: ["ReactJS", "PWA", "Weather API"],
+		demo: "https://hrushi-makode-weather-pwa.netlify.app/",
 		github: "https://github.com/HrushiMakode/PWA-React-App",
 		image: "https://i.ibb.co/hW1T0qb/Screenshot-9.png",
+	},
+	{
+		id: 4,
+		title: "Portfolio Website",
+		about:
+			"Responsive portfolio website using react to showcase my project and skills as a digital resume hosted on firebase using firebase hosting",
+		tags: ["ReactJS", "Firebase Hosting", "HTML", "CSS", "JS"],
+		demo: "https://hrushikesh-makode.web.app/",
+		github: "https://github.com/HrushiMakode/portfolio",
+		image: "https://i.ibb.co/bLPXmPc/my-react-portfolio-2x.png",
+	},
+	{
+		id: 5,
+		title: "Todo App",
+		about:
+			"Simple Todo-list web app to keep a track of things you gotta do, With fully local storage support without any backend support ",
+		tags: ["Angular", "RxJS", "Local Storage"],
+		demo: "https://hrushi-makode-ng-todo.netlify.app/",
+		github: "https://github.com/HrushiMakode/Angular-Todo",
+		image:
+			"https://raw.githubusercontent.com/HrushiMakode/Angular-Todo/master/.gitres/todo-angular%402x.png",
 	},
 ];
